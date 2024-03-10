a93b102bda3a91cb1129798ab8790ddc06d6029b
osman-butt <butt.osman@gmail.com>
2024-03-10 01:12:40 +0100

    Update Application.java

--- src/main/java/dk/Application.java
+++ src/main/java/dk/Application.java
@@ -2,10 +2,11 @@ package dk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 //Remove the exclude when/if security is needed
-//@SpringBootApplication( exclude = {SecurityAutoConfiguration.class} )
-@SpringBootApplication( )
+@SpringBootApplication( exclude = {SecurityAutoConfiguration.class} )
+//@SpringBootApplication( )
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
