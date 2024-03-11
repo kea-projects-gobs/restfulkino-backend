3c1b394be1cfccd34b49b80104ccad92762e0e7a
Benjamin-Harris1 <beha1503@gmail.com>
2024-03-11 18:43:51 +0100

    KINO-17 added real pictures to dummy cinemas

--- src/main/java/dk/kino/configuration/SetupCinemas.java
+++ src/main/java/dk/kino/configuration/SetupCinemas.java
@@ -26,9 +26,9 @@ public class SetupCinemas implements ApplicationRunner {
 
     private void createCinemas() {
         List<Cinema> cinemas = Arrays.asList(
-            new Cinema("Cinema Paradiso", "Springfield", "123 Main St", "A lovely place to watch films.", "123-456-7890", "info@cinemaparadiso.com", "http://example.com/image1.png"),
-            new Cinema("The Grand Screen", "Shelbyville", "456 Grand Ave", "Experience movies like never before.", "987-654-3210", "contact@thegrandscreen.com", "http://example.com/image2.png"),
-            new Cinema("Movie Palace", "Capital City", "789 Royal Rd", "The ultimate cinema experience.", "555-123-4567", "hello@moviepalace.com", "http://example.com/image3.png")
+            new Cinema("Cinema Paradiso", "Springfield", "123 Main St", "A lovely place to watch films.", "123-456-7890", "info@cinemaparadiso.com", "https://cms-assets.webediamovies.pro/cdn-cgi/image/dpr=1,fit=scale-down,gravity=auto,metadata=none,quality=85,width=1920,height=1131/production/2/519760e8e151087beb072aa43b20abd0.jpeg"),
+            new Cinema("The Grand Screen", "Shelbyville", "456 Grand Ave", "Experience movies like never before.", "987-654-3210", "contact@thegrandscreen.com", "https://cms-assets.webediamovies.pro/cdn-cgi/image/dpr=1,fit=scale-down,gravity=auto,metadata=none,quality=85,width=1920,height=1131/production/2/519760e8e151087beb072aa43b20abd0.jpeg"),
+            new Cinema("Movie Palace", "Capital City", "789 Royal Rd", "The ultimate cinema experience.", "555-123-4567", "hello@moviepalace.com", "https://cms-assets.webediamovies.pro/cdn-cgi/image/dpr=1,fit=scale-down,gravity=auto,metadata=none,quality=85,width=1920,height=1131/production/2/519760e8e151087beb072aa43b20abd0.jpeg")
         );
 
         cinemas.forEach(cinemaRepository::save);
