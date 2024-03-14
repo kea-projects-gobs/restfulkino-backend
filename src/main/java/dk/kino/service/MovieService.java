3c1688ce251d523a1bbd0c2f59656a2f81287e7f
osman-butt <butt.osman@gmail.com>
2024-03-14 03:38:59 +0100

    Made DTO mapper public

--- src/main/java/dk/kino/service/MovieService.java
+++ src/main/java/dk/kino/service/MovieService.java
@@ -1,6 +1,7 @@
 package dk.kino.service;
 
 import dk.kino.dto.MovieDTO;
+import dk.kino.entity.Movie;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,5 +13,5 @@ public interface MovieService {
     List<MovieDTO> findAll();
     MovieDTO update(int id, MovieDTO movieDTO);
     void delete(int id);
-
+    Movie toEntity(MovieDTO movieDTO);
 }
