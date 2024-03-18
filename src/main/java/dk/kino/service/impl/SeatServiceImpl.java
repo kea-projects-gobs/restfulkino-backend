a51228f500d27c1f0596600b0e8adaae76003c02
osman-butt <butt.osman@gmail.com>
2024-03-18 23:02:53 +0100

    Implemented SeatService methods

--- src/main/java/dk/kino/service/impl/SeatServiceImpl.java
+++ src/main/java/dk/kino/service/impl/SeatServiceImpl.java
@@ -1,24 +1,35 @@
 package dk.kino.service.impl;
 
 import dk.kino.entity.Seat;
+import dk.kino.repository.SeatRepository;
 import dk.kino.service.SeatService;
 
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class SeatServiceImpl implements SeatService {
+
+    private final SeatRepository seatRepository;
+    public SeatServiceImpl(SeatRepository seatRepository) {
+        this.seatRepository = seatRepository;
+    }
     @Override
-    public Seat createSeat(Seat Seat) {
-        return null;
+    public Seat createSeat(Seat seat) {
+        seat.setActive(true);
+        return seatRepository.save(seat);
     }
 
     @Override
-    public List<Seat> createSeats(Set<Seat> Seats) {
-        return null;
+    public List<Seat> createSeats(Set<Seat> seats) {
+        seats.forEach(seat -> seat.setActive(true));
+        return seatRepository.saveAll(seats);
     }
 
     @Override
-    public void deleteSeats(Set<Seat> Seats) {
-
+    public void deleteSeats(Set<Seat> seats) {
+        // Soft deletion
+        seats.forEach(seat -> seat.setActive(false));
+        seatRepository.saveAll(seats);
     }
 }
