c1afc7e182aac3db14b3e58eb9d7fe0f352a0aa5
osman-butt <butt.osman@gmail.com>
2024-03-19 23:47:56 +0100

    KINO-67 Renamed Endpoint

--- src/main/java/dk/kino/api/ReservationController.java
+++ src/main/java/dk/kino/api/ReservationController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/reservation")
+@RequestMapping("/api/reservations")
 public class ReservationController {
 
     private final ReservationService reservationService;
