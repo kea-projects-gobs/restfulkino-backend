5ac64b10d17055e3a039b99dd9ade1f7289481e6
osman-butt <butt.osman@gmail.com>
2024-03-19 23:30:51 +0100

    KINO-67 Created ReservationController

---
+++ src/main/java/dk/kino/api/ReservationController.java
@@ -0,0 +1,26 @@
+package dk.kino.api;
+
+import dk.kino.dto.ReservationReqDTO;
+import dk.kino.dto.ReservationResDTO;
+import dk.kino.service.ReservationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/reservation")
+public class ReservationController {
+
+    private final ReservationService reservationService;
+
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ReservationResDTO> createReservation(@RequestBody ReservationReqDTO reservationReqDTO){
+        return ResponseEntity.ok().body(reservationService.createReservation(reservationReqDTO));
+    }
+}
