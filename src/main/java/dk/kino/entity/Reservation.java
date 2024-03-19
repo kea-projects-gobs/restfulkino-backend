5b7f56ec5a61605b11e040dc345b9e77f5fd5a66
osman-butt <butt.osman@gmail.com>
2024-03-19 23:28:20 +0100

    KINO-67 Created Reservation entity+relation

---
+++ src/main/java/dk/kino/entity/Reservation.java
@@ -0,0 +1,27 @@
+package dk.kino.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @ManyToOne
+    @JoinColumn(name = "schedule_id")
+    private Schedule schedule;
+    @OneToMany(mappedBy = "reservation")
+    private Set<Ticket> tickets;
+    @Temporal(TemporalType.DATE)
+    private LocalDate reservationDate;
+    private double feeOrDiscount; // Fee (positive) or discount (negative) applied to the reservation
+}
